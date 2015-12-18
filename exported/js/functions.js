1e2ff7362d5fbf76c6ac5620679ed14d87868fa5
direbacon <bhuggins.fl@gmail.com>
2015-12-17 23:23:59 -0600

    Break Element Setup
    Parallaxing is working on all break elements

--- exported/js/functions.js
+++ exported/js/functions.js
@@ -1,24 +1,42 @@
+//Readying functions for user imputs
 $(function() {
     console.log('Page is Loaded');
     smoothScroll(1000);
     aboutSwitch();
 })
 
+
+//Functions based on scroll position of window
 $(window).scroll(function(){
     //Collect the top and bottom edge of browser window
     var wScrollT = $(this).scrollTop();
     var wScrollB = $(this).scrollTop() + $(window).height();
-    console.log('Top of Window: ' +wScrollT);
-    //Transform header to compact version once off of landing page
+    var aboutT = $('.about-brk').offset().top;
+    var portT = $('.portfolio-brk').offset().top;
+    var resT = $('.resume-brk').offset().top;
+    //console.log('Top of Window: ' +wScrollT);
+    
+    //Controlling Header's morph from full to compact and back
     if(wScrollT > ($('.landing').height() - 435)){
         $('header').addClass('compact');
     } else {
         $('header').removeClass('compact');
     }
     
-    
+    //Contorlling parallax effects of break images
+    if(wScrollB > (aboutT-10)){
+        $('.about-brk').css({'background-position':'center ' + -(((wScrollT - aboutT)/8)+($(window).width()/10)) + 'px'})
+    }
+    if(wScrollB > (portT-10)){
+        $('.portfolio-brk').css({'background-position':'center ' + -(((wScrollT - portT)/8)+($(window).width()/10)) + 'px'})
+    }
+    if(wScrollB > (resT-10)){
+        $('.resume-brk').css({'background-position':'center ' + -(((wScrollT - resT)/8)+($(window).width()/10)) + 'px'})
+    }
 })
 
+
+//Controlling the scrolling of the page when clicking on a nav item 
 function smoothScroll(dur) {
     $('a[href^="#"]').on('click', function(event){
         var target = $($(this).attr('href'));
@@ -32,6 +50,7 @@ function smoothScroll(dur) {
     });
 }
 
+//Controlling the styles and content of the About section
 function aboutSwitch() {
     console.log('Transition is ready');
     $('.personal').click(function(){
